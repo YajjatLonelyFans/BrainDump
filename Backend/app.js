f1abe2d146af516c2c6799655962cb11b8c64cfe
LILCODAH <rajkumar14ailawadi@gmail.com>
2025-11-04 17:22:55 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -14,7 +14,7 @@ app.use("/notes" , noteRoutes);
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-})
+})  
 .then(() => console.log("✅ MongoDB connected successfully"))
 .catch((err) => console.error("❌ MongoDB connection error:", err));
 
